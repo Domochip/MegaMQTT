89061b2139ba9046b1ac137c5bd1e8a1338533c9
DomoChip <domochip@hotmail.com>
2019-04-24 22:23:53 +0200

    Add VerySimpleTimer class

---
+++ src/VerySimpleTimer.cpp
@@ -0,0 +1,35 @@
+#include "VerySimpleTimer.h"
+
+void VerySimpleTimer::SetOnceTimeout(uint16_t d)
+{
+    _once = true;
+    SetTimeout(d);
+};
+void VerySimpleTimer::SetTimeout(uint16_t d)
+{
+    _timeoutDuration = d;
+    Reset();
+};
+void VerySimpleTimer::Reset()
+{
+    _nextMillis = millis() + _timeoutDuration;
+    _active = true;
+};
+bool VerySimpleTimer::IsTimeoutOver()
+{
+    if (_active && millis() > _nextMillis)
+    {
+        if (_once)
+            _active = false;
+        else
+            _nextMillis += _timeoutDuration;
+
+        return true;
+    }
+    return false;
+};
+
+bool VerySimpleTimer::IsActive()
+{
+    return _active;
+};
---
+++ src/VerySimpleTimer.h
@@ -0,0 +1,22 @@
+#ifndef VerySimpleTimer_h
+#define VerySimpleTimer_h
+
+#include <Arduino.h>
+
+class VerySimpleTimer
+{
+  private:
+    bool _active = false;
+    bool _once = false;
+    uint16_t _timeoutDuration = 0;
+    unsigned long _nextMillis = 0;
+
+  public:
+    void SetOnceTimeout(uint16_t d);
+    void SetTimeout(uint16_t d);
+    void Reset();
+    bool IsTimeoutOver();
+    bool IsActive();
+};
+
+#endif
